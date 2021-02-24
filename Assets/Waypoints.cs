5c30b69927974ceb18b343faf18fde9d291e61c5
adam6070 <mighty_fawzi@hotmail.com>
2021-02-24 18:27:07 +0200

    added enemies + waypoints + wave-spawner + turrets

--- Assets/Waypoints.cs
+++ Assets/Waypoints.cs
@@ -2,5 +2,14 @@
 
 public class Waypoints : MonoBehaviour
 {
-   
+    public static Transform[] points;
+
+    private void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
 }
